efbbae5ceef29c68a1b2d735d14c13fce21f9e69
wldnjs7064 <wldnjs7064@naver.com>
2023-05-04 14:24:19 +0900

    added user detail

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -2,6 +2,8 @@ import React,{useRef, useState} from 'react'
 import {Link} from 'react-router-dom';
 import {useForm} from 'react-hook-form';
 import firebase from '../../firebase';
+import md5 from 'md5';
+
 function RegisterPage() {
     const {register,watch, formState:{errors}, handleSubmit} = useForm();
 	const [errorFromSubmit, setErrorFromSubmit] = useState("")
@@ -17,6 +19,13 @@ function RegisterPage() {
 				.auth()
 				.createUserWithEmailAndPassword(data.email,data.password)
 			console.log('createdUser',createdUser)
+
+			await createdUser.user.updateProfile({
+				displayName: data.name,
+				photoURL: 'http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon'
+			})
+
+
 			setLoading(false)
 		}catch(error){
 			setErrorFromSubmit(error.message)
