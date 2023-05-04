6efa2d9c93f6326d0382118de848d03239586461
wldnjs7064 <wldnjs7064@naver.com>
2023-05-04 15:28:41 +0900

    login Routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Routes, Route, useNavigate} from "react-router-dom";
 import ChatPage from "./components/ChatPage/ChatPage";
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import { useEffect } from "react";
+import firebase from './firebase';
 function App() {
+  let navigate = useNavigate();
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log('user', user)
+      //로그인이 된 상태
+      if (user)
+      {
+        navigate("/");
+      }
+      else {
+        navigate("/login");
+      }
+    })
+  }, [])
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<ChatPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
-    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {createStore, applyMiddleware} from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 import Reducer from './redux/reducers';
-
+import { BrowserRouter as Router } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
@@ -18,7 +18,9 @@ root.render(
     <Provider store={createStoreWithMiddleware(Reducer,
       window.__REDUX_DEVTOOLS_EXTENSION__ &&
       window.__REDUX_DEVTOOLS_EXTENSION__)}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
