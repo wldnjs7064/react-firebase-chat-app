68e6084bbf204abb1566b22faa45124db513ca8c
wldnjs7064 <wldnjs7064@naver.com>
2023-05-22 02:09:25 +0900

    login/register ui 1차 완료

--- src/assets/index.js
+++
@@ -1,3 +0,0 @@
-export { ReactComponent as ColoredLogo } from "../assets/svg/ColoredLogo.svg";
-export { ReactComponent as ProfileIcon } from "../assets/svg/ProfileIcon.svg";
-export { ReactComponent as ForwardArrow } from "../assets/svg/forwardArrow.svg";
--- src/firebase.js
+++ src/firebase.js
@@ -5,16 +5,16 @@ import "firebase/compat/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAE66f1cPMYx74zlkC30txOhndXa5vQrXs",
     authDomain: "react-firebase-chat-app-e96b2.firebaseapp.com",
+    databaseURL: "https://react-firebase-chat-app-e96b2-default-rtdb.firebaseio.com",
     projectId: "react-firebase-chat-app-e96b2",
     storageBucket: "react-firebase-chat-app-e96b2.appspot.com",
     messagingSenderId: "169087505826",
-    appId: "1:169087505826:web:cf61667813f0923c49cefb",
-    measurementId: "G-F1Q89JMY3M"
+    appId: "1:169087505826:web:e85943c0fd9682d349cefb",
+    measurementId: "G-QJKK6NZ2ZS"
   };
-
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
