4d1ac306d4301e012bcaa0f7b9abff014e5404ea
wldnjs7064 <wldnjs7064@naver.com>
2023-05-23 14:15:16 +0900

    Fix:Firebase App 변경

--- src/firebase.js
+++ src/firebase.js
@@ -8,11 +8,12 @@ import "firebase/compat/storage";
 var firebaseConfig = {
     apiKey: "AIzaSyAE66f1cPMYx74zlkC30txOhndXa5vQrXs",
     authDomain: "react-firebase-chat-app-e96b2.firebaseapp.com",
+    databaseURL: "https://react-firebase-chat-app-e96b2-default-rtdb.firebaseio.com",
     projectId: "react-firebase-chat-app-e96b2",
     storageBucket: "react-firebase-chat-app-e96b2.appspot.com",
     messagingSenderId: "169087505826",
-    appId: "1:169087505826:web:cf61667813f0923c49cefb",
-    measurementId: "G-F1Q89JMY3M"
+    appId: "1:169087505826:web:b6ba87feec079ed649cefb",
+    measurementId: "G-PN9VD5GDLH"
   };
 
   // Initialize Firebase
