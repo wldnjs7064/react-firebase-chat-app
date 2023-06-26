ae7cb520a02d69720cda014b4a472c93dee56245
wldnjs7064 <wldnjs7064@naver.com>
2023-06-26 12:58:50 +0900

    로그인 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
 import MainPage from "./components/MainPage/MainPage";
-import LoginPage from './components/LoginPage/LoginPage';
-import RegisterPage from './components/RegisterPage/RegisterPage';
-import ChatPage from './components/ChatPage/ChatPage';
+import LoginPage from "./components/LoginPage/LoginPage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
+import ChatPage from "./components/ChatPage/ChatPage";
+import firebase from "./firebase";
 
 function App() {
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        //로그인 시에 메인페이지로 이동
+        //console.log(user);
+        navigate("/");
+      } else {
+        navigate("/login");
+      }
+    });
+  }, []);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/chat" element={<ChatPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware} from 'redux';
-import promiseMiddleware from 'redux-promise';
-import ReduxThunk from 'redux-thunk';
-import Reducer from './redux/reducers';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./redux/reducers";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
-const createStoreWithMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore)
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={createStoreWithMiddleware(Reducer,
-      window.__REUDX_DEVTOOLS_EXTENSION__ &&
-      window.__REUDX_DEVTOOLS_EXTENSION__()
-    )}>
-      <App />
+    <Provider
+      store={createStoreWithMiddleware(
+        Reducer,
+        window.__REUDX_DEVTOOLS_EXTENSION__ &&
+          window.__REUDX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
