1f159f97c3350a3f160e819eb29d4eb858964a01
hyorish03 <hyorish2001@gmail.com>
2023-07-07 12:06:42 +0900

    Feat: 글쓰기 기능 추가

--- src/components/MainPage/Body/Board/Write.jsx
+++ src/components/MainPage/Body/Board/Write.jsx
@@ -1,12 +1,37 @@
 import ToastEditor from "components/ToastEditor";
 import React from "react";
-
+import styled from "styled-components";
+import * as S from "../../style";
+import Header from "components/MainPage/Header/Header";
 function BoardWrite() {
   return (
     <div>
-      <ToastEditor />
+      <Header style={{ backgroundColor: "fafafa" }} />
+      <S.Body>
+        <Wrapper>
+          <RegisterButton>등록하기</RegisterButton>
+          <ToastEditor />
+        </Wrapper>
+      </S.Body>
     </div>
   );
 }
+
+const RegisterButton = styled.button`
+  overflow: hidden;
+  background-color: gray;
+  color: white;
+  border: solid;
+  min-width: 64px;
+  height: 40px;
+`;
+const Wrapper = styled.div`
+  width: fit-content;
+  height: fit-content;
+  padding: 10px;
+  align-content: center;
+  gap: 15px;
+  background-color: #fafafa;
+`;
 
 export default BoardWrite;
