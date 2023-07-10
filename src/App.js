d9365f417363121cfa93d22dcda6ea721a9aaa9c
hyorish03 <hyorish2001@gmail.com>
2023-07-10 19:17:32 +0900

    회원가입시 에러 보여주기, 완료시 alert 및 라우팅

--- src/App.js
+++ src/App.js
@@ -7,35 +7,37 @@ import ChatPage from "./components/ChatPage/ChatPage";
 import firebase from "./firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "./redux/actions/user_action";
+
 function App() {
   let navigate = useNavigate();
   let dispatch = useDispatch();
   const isLoading = useSelector((state) => state.user.isLoading);
+
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         //로그인 시에 메인페이지로 이동
         //console.log(user);
-        navigate("/");
+        // navigate("/");
         dispatch(setUser(user));
       } else {
-        navigate("/login");
+        // navigate("/login");
       }
     });
   }, []);
 
-  if (isLoading) {
-    return <div>로딩중입니다.</div>;
-  } else {
-    return (
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    );
-  }
+  // if (isLoading) {
+  //   return <div>로딩중입니다.</div>;
+  // } else {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/chat" element={<ChatPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
+  );
 }
+// }
 
 export default App;
