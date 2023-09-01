d8aa89875d8bae0b4266d8c0836e961b645f5b46
wldnjs7064 <wldnjs7064@naver.com>
2023-09-01 11:31:39 +0900

    feat: 프로필 사진 변경 기능 구현

--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { IoIosChatboxes } from "react-icons/io";
 import Dropdown from "react-bootstrap/Dropdown";
 import Image from "react-bootstrap/Image";
@@ -7,8 +7,12 @@ import firebase from "../../../firebase";
 
 function UserPanel() {
   const user = useSelector((state) => state.user.currentUser);
+  const inputOpenImageRef = useRef();
   const handleLogout = () => {
     firebase.auth().signOut();
+  };
+  const handleOpenImageRef = () => {
+    inputOpenImageRef.current.click();
   };
   return (
     <div>
@@ -30,11 +34,19 @@ function UserPanel() {
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">프로필 사진 변경</Dropdown.Item>
+            <Dropdown.Item onClick={handleOpenImageRef}>
+              프로필 사진 변경
+            </Dropdown.Item>
             <Dropdown.Item onClick={handleLogout}>로그아웃</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </div>
+      <input
+        accept="image/jpeg, image/png"
+        style={{ display: "none" }}
+        ref={inputOpenImageRef}
+        type="file"
+      />
     </div>
   );
 }
