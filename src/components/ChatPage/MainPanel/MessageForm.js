67026b29782c7d540574489530ec8c1e7129d27f
wldnjs7064 <wldnjs7064@naver.com>
2023-09-08 14:02:10 +0900

    feat: 메시지 messages db에 저장

--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -1,21 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import ProgressBar from "react-bootstrap/ProgressBar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import firebase from "../../../firebase";
+import { useSelector } from "react-redux";
 function MessageForm() {
+  const chatRoom = useSelector((state) => state.chatRoom.currentChatRoom);
+  const user = useSelector((state) => state.user.currentUser);
+
+  const [content, setContent] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const messagesRef = firebase.database().ref("messages");
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+  const createMessage = (fileURL = null) => {
+    const message = {
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      user: {
+        id: user.uid,
+        name: user.displayName,
+        image: user.photoURL,
+      },
+    };
+    if (fileURL !== null) {
+      message["image"] = fileURL;
+    } else {
+      message["content"] = content;
+    }
+    return message;
+  };
+  const handleSubmit = async () => {
+    if (!content) {
+      setErrors((prev) => prev.concat("Type contents first"));
+      return;
+    }
+    setLoading(true);
+
+    try {
+      await messagesRef.child(chatRoom.id).push().set(createMessage());
+      setLoading(false);
+      setContent("");
+      setErrors([]);
+    } catch (error) {
+      setErrors((pre) => pre.concat(error.message));
+      setLoading(false);
+      setTimeout(() => {
+        setErrors([]);
+      }, 5000);
+    }
+  };
   return (
     <div>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control
+            value={content}
+            onChange={handleChange}
+            as="textarea"
+            rows={3}
+          />
         </Form.Group>
       </Form>
 
       <ProgressBar variant="warning" label="60%" now={60} />
+      <div>
+        {errors.map((errorMsg) => (
+          <p style={{ color: "red" }} key={errorMsg}>
+            {errorMsg}
+          </p>
+        ))}
+      </div>
       <Row>
         <Col>
-          <button className="message-form-button" style={{ width: "100%" }}>
+          <button
+            onClick={handleSubmit}
+            className="message-form-button"
+            style={{ width: "100%" }}
+          >
             SEND
           </button>
         </Col>
