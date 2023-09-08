3f45071e2c75519946015185bb36332a1ac2b38e
wldnjs7064 <wldnjs7064@naver.com>
2023-09-08 13:32:54 +0900

    feat: message form 부분 UI

--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -1,7 +1,32 @@
 import React from "react";
-
+import Form from "react-bootstrap/Form";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 function MessageForm() {
-  return <div>MessageForm</div>;
+  return (
+    <div>
+      <Form>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
+
+      <ProgressBar variant="warning" label="60%" now={60} />
+      <Row>
+        <Col>
+          <button className="message-form-button" style={{ width: "100%" }}>
+            SEND
+          </button>
+        </Col>
+        <Col>
+          <button className="message-form-button" style={{ width: "100%" }}>
+            UPLOAD
+          </button>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default MessageForm;
