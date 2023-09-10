2d66a5ffa534779deddac8070c4df1b009e5efb6
wldnjs7064 <wldnjs7064@naver.com>
2023-09-10 11:54:55 +0900

    Feat: 메시지 화면에 보여주기

--- src/components/ChatPage/ChatPage.js
+++ src/components/ChatPage/ChatPage.js
@@ -2,15 +2,19 @@ import React from "react";
 import SidePanel from "./SidePanel/SidePanel";
 import MainPanel from "./MainPanel/MainPanel";
 import Header from "components/MainPage/Header/Header"; //헤더 나중에 넣을거 생각
+import { useSelector } from "react-redux";
 
 function ChatPage() {
+  const currentChatRoom = useSelector(
+    (state) => state.chatRoom.currentChatRoom
+  );
   return (
     <div style={{ display: "flex" }}>
       <div style={{ width: "300px" }}>
         <SidePanel />
       </div>
       <div style={{ width: "100%" }}>
-        <MainPanel />
+        <MainPanel key={currentChatRoom && currentChatRoom.id} />
       </div>
     </div>
   );
--- src/components/ChatPage/MainPanel/MainPanel.js
+++ src/components/ChatPage/MainPanel/MainPanel.js
@@ -1,10 +1,43 @@
 import React, { Component } from "react";
 import MessageHeader from "./MessageHeader";
 import Message from "./Message";
+import { connect } from "react-redux";
 import MessageForm from "./MessageForm";
+import firebase from "../../../firebase";
 
 export class MainPanel extends Component {
+  state = {
+    messages: [],
+    messagesRef: firebase.database().ref("messages"),
+    messagesLoading: true,
+  };
+  componentDidMount() {
+    const { chatRoom } = this.props;
+    if (chatRoom) {
+      this.addMessagesListeners(chatRoom.id);
+    }
+  }
+
+  addMessagesListeners = (chatRoomId) => {
+    let messagesArray = [];
+    this.state.messagesRef
+      .child(chatRoomId)
+      .on("child_added", (DataSnapshot) => {
+        messagesArray.push(DataSnapshot.val());
+        this.setState({ messages: messagesArray, messagesLoading: false });
+      });
+  };
+  renderMessages = (messages) =>
+    messages.length > 0 &&
+    messages.map((message) => (
+      <Message
+        key={message.timestamp}
+        message={message}
+        user={this.props.user}
+      />
+    ));
   render() {
+    const { messages } = this.state;
     return (
       <div style={{ padding: "2rem 2rem 0 2rem" }}>
         <MessageHeader />
@@ -19,11 +52,20 @@ export class MainPanel extends Component {
             marginBottom: "1rem",
             overflowY: "auto",
           }}
-        ></div>
+        >
+          {this.renderMessages(messages)}
+        </div>
         <MessageForm />
       </div>
     );
   }
 }
 
-export default MainPanel;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user.currentUser,
+    chatRoom: state.chatRoom.currentChatRoom,
+  };
+};
+
+export default connect(mapStateToProps)(MainPanel);
