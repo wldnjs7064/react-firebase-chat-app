5c537cfa6e6b53068d48d3e61e890d33d4a357a6
wldnjs7064 <wldnjs7064@naver.com>
2023-09-17 14:43:40 +0900

    Feat: User 목록 가져오기

--- src/components/ChatPage/SidePanel/DirectMessages.js
+++ src/components/ChatPage/SidePanel/DirectMessages.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
+import { FaRegSmile } from "react-icons/fa";
+import firebase from "../../../firebase";
+import { connect } from "react-redux";
+export class DirectMessages extends Component {
+  state = {
+    usersRef: firebase.database().ref("users"),
+    users: [],
+  };
+  componentDidMount() {
+    if (this.props.user) {
+      this.addUsersListeners(this.props.user.uid);
+    }
+  }
+  addUsersListeners = (currentUserId) => {
+    const { usersRef } = this.state;
+    let usersArray = [];
+    usersRef.on("child_added", (DataSnapshot) => {
+      if (currentUserId !== DataSnapshot.key) {
+        // console.log("Datasnapshot.val()", DataSnapshot.val());
+        let user = DataSnapshot.val();
+        user["uid"] = DataSnapshot.key;
+        user["status"] = "offline";
+        usersArray.push(user);
+        this.setState({ users: usersArray });
+      }
+    });
+  };
+  renderDirectMessages = (users) =>
+    users.length > 0 &&
+    users.map((user) => <li key={user.uid}>#{user.name} </li>);
+  render() {
+    const { users } = this.state;
+    return (
+      <div>
+        <span style={{ display: "flex", alignItems: "center" }}>
+          <FaRegSmile style={{ marginRight: 3 }} /> DIRECT MESSAGES(1)
+        </span>
 
-function DirectMessages() {
-  return <div>DirectMessages</div>;
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          {this.renderDirectMessages(users)}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default DirectMessages;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user.currentUser,
+  };
+};
+export default connect()(DirectMessages);
