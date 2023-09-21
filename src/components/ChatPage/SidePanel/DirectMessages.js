a463f04036ad80e55759b84bdbee349ddf3e0603
wldnjs7064 <wldnjs7064@naver.com>
2023-09-21 14:41:44 +0900

    feat: 유저 이름 클릭 시 해당 유저와 방 Id 생성

--- src/components/ChatPage/SidePanel/DirectMessages.js
+++ src/components/ChatPage/SidePanel/DirectMessages.js
@@ -26,9 +26,23 @@ export class DirectMessages extends Component {
       }
     });
   };
+  getChatRoomId = (userId) => {
+    const currentUserId = this.props.uid;
+
+    return userId > currentUserId
+      ? `${userId}/${currentUserId}`
+      : `${currentUserId}/${userId}`;
+  };
+  changeChatRoom = (user) => {
+    const chatRoomId = this.getChatRoomId(user.uid);
+  };
   renderDirectMessages = (users) =>
     users.length > 0 &&
-    users.map((user) => <li key={user.uid}>#{user.name} </li>);
+    users.map((user) => (
+      <li key={user.uid} onClick={this.changeChatRoom(user)}>
+        # {user.name}{" "}
+      </li>
+    ));
   render() {
     const { users } = this.state;
     return (
