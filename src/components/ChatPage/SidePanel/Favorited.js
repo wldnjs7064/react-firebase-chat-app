8769af1934bb8d950f46046eef8d6fea3ff93dd3
wldnjs7064 <wldnjs7064@naver.com>
2023-09-24 15:10:52 +0900

    feat: 즐겨찾기 방 리스트 UI 구현

--- src/components/ChatPage/SidePanel/Favorited.js
+++ src/components/ChatPage/SidePanel/Favorited.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { FaRegSmileBeam } from "react-icons/fa";
 
 function Favorited() {
-  return <div>Favorited</div>;
+  return (
+    <div>
+      <span style={{ display: "flex", alignItems: "center" }}>
+        <FaRegSmileBeam style={{ marginRight: "3px" }} />
+        FVORITED (1)
+      </span>
+      <ul style={{ listStyleType: "none", padding: "0" }}></ul>
+    </div>
+  );
 }
 
 export default Favorited;
