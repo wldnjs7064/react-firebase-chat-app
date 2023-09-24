a1d99c80ac50c2d20529e95f030e6f24cfa8159a
wldnjs7064 <wldnjs7064@naver.com>
2023-09-24 16:35:12 +0900

    feat: 채팅방 즐겨찾기 리스트에 채팅방 추가

--- src/components/ChatPage/SidePanel/Favorited.js
+++ src/components/ChatPage/SidePanel/Favorited.js
@@ -1,16 +1,98 @@
-import React from "react";
 import { FaRegSmileBeam } from "react-icons/fa";
+import React, { Component } from "react";
+import firebase from "../../../firebase";
+import { connect } from "react-redux";
+import {
+  setCurrentChatRoom,
+  setPrivateChatRoom,
+} from "redux/actions/chatRoom_action";
 
-function Favorited() {
-  return (
-    <div>
-      <span style={{ display: "flex", alignItems: "center" }}>
-        <FaRegSmileBeam style={{ marginRight: "3px" }} />
-        FVORITED (1)
-      </span>
-      <ul style={{ listStyleType: "none", padding: "0" }}></ul>
-    </div>
-  );
+export class Favorited extends Component {
+  state = {
+    favoritedChatRoom: [],
+    usersRef: firebase.database().ref("users"),
+    activeChatRoomId: "",
+  };
+  componentDidMount() {
+    if (this.props.user) {
+      this.addListeners(this.props.user.uid);
+    }
+  }
+
+  addListeners = (userId) => {
+    const { usersRef } = this.state;
+    usersRef
+      .child(userId)
+      .child("favorited")
+      .on("child_added", (DataSnapshot) => {
+        const favoritedChatRoom = {
+          id: DataSnapshot.key,
+          ...DataSnapshot.val(),
+        };
+        this.setState({
+          favoritedChatRoom: [
+            ...this.state.favoritedChatRoom,
+            favoritedChatRoom,
+          ],
+        });
+      });
+    usersRef
+      .child(userId)
+      .child("favorited")
+      .on("child_removed", (DataSnapshot) => {
+        const chatRoomToRemove = {
+          id: DataSnapshot.key,
+          ...DataSnapshot.val(),
+        };
+        const filteredChatRooms = this.state.favoritedChatRoom.filter(
+          (chatRoom) => {
+            return chatRoom.id !== chatRoomToRemove.id;
+          }
+        );
+        this.setState({ favoritedChatRoom: filteredChatRooms });
+      });
+  };
+
+  changeChatRoom = (room) => {
+    this.props.dispatch(setCurrentChatRoom(room));
+    this.props.dispatch(setPrivateChatRoom(false));
+    this.setState({ activeChatRoomId: room.id });
+  };
+
+  renderFavoritedChatRooms = (favoritedChatRoom) =>
+    favoritedChatRoom.length > 0 &&
+    favoritedChatRoom.map((chatRoom) => (
+      <li
+        key={chatRoom.id}
+        onClick={() => this.changeChatRoom(chatRoom)}
+        style={{
+          backgroundColor:
+            chatRoom.id === this.state.activeChatRoomId && "#ffffff45",
+        }}
+      >
+        # {chatRoom.name}
+      </li>
+    ));
+
+  render() {
+    const { favoritedChatRoom } = this.state;
+    return (
+      <div>
+        <span style={{ display: "flex", alignItems: "center" }}>
+          <FaRegSmileBeam style={{ marginRight: "3px" }} />
+          FVORITED (1)
+        </span>
+        <ul style={{ listStyleType: "none", padding: "0" }}>
+          {this.renderFavoritedChatRooms(favoritedChatRoom)}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default Favorited;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user.currentUser,
+  };
+};
+export default connect(mapStateToProps)(Favorited);
