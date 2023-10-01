c52afb8d77d3a45bff7bd4b1b897722bb98179d7
wldnjs7064 <wldnjs7064@naver.com>
2023-10-02 00:16:00 +0900

    fix: firebase 수정

--- src/firebase.js
+++ src/firebase.js
@@ -6,17 +6,17 @@ import "firebase/compat/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyAE66f1cPMYx74zlkC30txOhndXa5vQrXs",
-    authDomain: "react-firebase-chat-app-e96b2.firebaseapp.com",
-    databaseURL: "https://react-firebase-chat-app-e96b2-default-rtdb.firebaseio.com",
-    projectId: "react-firebase-chat-app-e96b2",
-    storageBucket: "react-firebase-chat-app-e96b2.appspot.com",
-    messagingSenderId: "169087505826",
-    appId: "1:169087505826:web:b6ba87feec079ed649cefb",
-    measurementId: "G-PN9VD5GDLH"
-  };
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURE_ID,
+};
 
-  // Initialize Firebase
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;
